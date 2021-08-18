369b4166271ec35696b578b97b903c841ba5da88
Bruce Lee <axengine@gmail.com>
2021-08-18 14:28:54 +0800

    rename erc20 abi

--- orc20.go
+++ orc20.go
@@ -10,10 +10,10 @@ import (
 	"strings"
 )
 
-var erc20abi = `[{"constant":true,"inputs":[],"name":"name","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_spender","type":"address"},{"name":"_value","type":"uint256"}],"name":"approve","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"totalSupply","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_from","type":"address"},{"name":"_to","type":"address"},{"name":"_value","type":"uint256"}],"name":"transferFrom","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"decimals","outputs":[{"name":"","type":"uint8"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"_who","type":"address"}],"name":"balanceOf","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"symbol","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_to","type":"address"},{"name":"_value","type":"uint256"}],"name":"transfer","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"_owner","type":"address"},{"name":"_spender","type":"address"}],"name":"allowance","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"inputs":[{"name":"_name","type":"string"},{"name":"_symbol","type":"string"},{"name":"_decimals","type":"uint8"}],"payable":false,"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":true,"name":"owner","type":"address"},{"indexed":true,"name":"spender","type":"address"},{"indexed":false,"name":"value","type":"uint256"}],"name":"Approval","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"from","type":"address"},{"indexed":true,"name":"to","type":"address"},{"indexed":false,"name":"value","type":"uint256"}],"name":"Transfer","type":"event"}]`
+var openzeppelinERC20Abi = `[{"constant":true,"inputs":[],"name":"name","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_spender","type":"address"},{"name":"_value","type":"uint256"}],"name":"approve","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"totalSupply","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_from","type":"address"},{"name":"_to","type":"address"},{"name":"_value","type":"uint256"}],"name":"transferFrom","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"decimals","outputs":[{"name":"","type":"uint8"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"_who","type":"address"}],"name":"balanceOf","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"symbol","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_to","type":"address"},{"name":"_value","type":"uint256"}],"name":"transfer","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"_owner","type":"address"},{"name":"_spender","type":"address"}],"name":"allowance","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"inputs":[{"name":"_name","type":"string"},{"name":"_symbol","type":"string"},{"name":"_decimals","type":"uint8"}],"payable":false,"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":true,"name":"owner","type":"address"},{"indexed":true,"name":"spender","type":"address"},{"indexed":false,"name":"value","type":"uint256"}],"name":"Approval","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"from","type":"address"},{"indexed":true,"name":"to","type":"address"},{"indexed":false,"name":"value","type":"uint256"}],"name":"Transfer","type":"event"}]`
 
 func (cli *ETHCli) ORC20Name(token string) (string, error) {
-	ins, err := abi.JSON(strings.NewReader(erc20abi))
+	ins, err := abi.JSON(strings.NewReader(openzeppelinERC20Abi))
 	if err != nil {
 		return "", err
 	}
@@ -37,7 +37,7 @@ func (cli *ETHCli) ORC20Name(token string) (string, error) {
 }
 
 func (cli *ETHCli) ORC20Symbol(token string) (string, error) {
-	ins, err := abi.JSON(strings.NewReader(erc20abi))
+	ins, err := abi.JSON(strings.NewReader(openzeppelinERC20Abi))
 	if err != nil {
 		return "", err
 	}
@@ -61,7 +61,7 @@ func (cli *ETHCli) ORC20Symbol(token string) (string, error) {
 }
 
 func (cli *ETHCli) ORC20Decimals(token string) (uint8, error) {
-	ins, err := abi.JSON(strings.NewReader(erc20abi))
+	ins, err := abi.JSON(strings.NewReader(openzeppelinERC20Abi))
 	if err != nil {
 		return 0, err
 	}
@@ -85,7 +85,7 @@ func (cli *ETHCli) ORC20Decimals(token string) (uint8, error) {
 }
 
 func (cli *ETHCli) ORC20TotalSupply(token string) (*big.Int, error) {
-	ins, err := abi.JSON(strings.NewReader(erc20abi))
+	ins, err := abi.JSON(strings.NewReader(openzeppelinERC20Abi))
 	if err != nil {
 		return nil, err
 	}
@@ -109,7 +109,7 @@ func (cli *ETHCli) ORC20TotalSupply(token string) (*big.Int, error) {
 }
 
 func (cli *ETHCli) ORC20BalanceOf(token string, address string) (*big.Int, error) {
-	ins, err := abi.JSON(strings.NewReader(erc20abi))
+	ins, err := abi.JSON(strings.NewReader(openzeppelinERC20Abi))
 	if err != nil {
 		return nil, err
 	}
@@ -133,7 +133,7 @@ func (cli *ETHCli) ORC20BalanceOf(token string, address string) (*big.Int, error
 }
 
 func (cli *ETHCli) ORC20Transfer(key, token, to, value string) (string, error) {
-	ins, err := abi.JSON(strings.NewReader(erc20abi))
+	ins, err := abi.JSON(strings.NewReader(openzeppelinERC20Abi))
 	if err != nil {
 		return "", err
 	}
@@ -147,7 +147,7 @@ func (cli *ETHCli) ORC20Transfer(key, token, to, value string) (string, error) {
 }
 
 func (cli *ETHCli) ORC20Allowance(token, owner, spender string) (*big.Int, error) {
-	ins, err := abi.JSON(strings.NewReader(erc20abi))
+	ins, err := abi.JSON(strings.NewReader(openzeppelinERC20Abi))
 	if err != nil {
 		return nil, err
 	}
@@ -171,7 +171,7 @@ func (cli *ETHCli) ORC20Allowance(token, owner, spender string) (*big.Int, error
 }
 
 func (cli *ETHCli) ORC20TransferFrom(key, token, from, to, value string) (string, error) {
-	ins, err := abi.JSON(strings.NewReader(erc20abi))
+	ins, err := abi.JSON(strings.NewReader(openzeppelinERC20Abi))
 	if err != nil {
 		return "", err
 	}
@@ -185,7 +185,7 @@ func (cli *ETHCli) ORC20TransferFrom(key, token, from, to, value string) (string
 }
 
 func (cli *ETHCli) ORC20Approve(key, token, spender, value string) (string, error) {
-	ins, err := abi.JSON(strings.NewReader(erc20abi))
+	ins, err := abi.JSON(strings.NewReader(openzeppelinERC20Abi))
 	if err != nil {
 		return "", err
 	}
